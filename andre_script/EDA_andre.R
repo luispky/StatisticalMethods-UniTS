e93f0ae936ce3b64e06ab7d1ff881205705057c8
Andrea Gottardi <andreagottardi24@gmail.com>
2024-01-30 14:08:58 +0100

    AIC analysis of variables

--- andre_script/EDA_andre.R
+++ andre_script/EDA_andre.R
@@ -6,6 +6,10 @@ setwd("Desktop/uni/statistical_methods/final_project/stats_project")
 
 load(file='datasets/test_data.RData')
 load(file='datasets/train_data.RData')
+colnames(train_data)
+df <- select(train_data, -Region_Code, -Policy_Sales_Channel)
+colnames(df)
+
 
 library(ggplot2); library(car); library(mgcv); library(skimr); library(viridis)
 library(psych); library(gridExtra); library(dplyr); library(skimr)
@@ -423,9 +427,69 @@ summary(mod)
 
 #### VARIABLES IMPORTANCE ####
 
-names <- colnames(train_data)
+# sort var by importance with AIC
+names <- colnames(df)
+names <- names[names != 'Response']
 models <- list()
+
+predictors = c("Annual_Premium", "s(log(Age))")
+for variable in predictors
+model_predictos = predictos without variable
+predictors_symols = sym(model_preditors)
+gam(Response ~ sum(predcitors))
+
 for (name in names) {
-  model <- glm(Response~.-!!sym(name), data=train_data, family=binomial)
-  models[[col_name]] <- model
+  print(name)
+  formula_string <- paste("Response ~ . -", name)
+  model_formula <- as.formula(formula_string)
+  model <- glm(model_formula, data = df, family = binomial)
+  print(model$aic)
+  models[[name]] <- model
 }
+
+
+
+for (name in names) {
+  print(name)
+  if name is not a numerical var
+    model = glm(Response ~ sum of categorical - name + log(numerical vars))
+  else
+    model = glm(Response ~ )
+  
+  formula_string <- paste("Response ~ . -", name)
+  model_formula <- as.formula(formula_string)
+  model <- glm(model_formula, data = df, family = binomial)
+  print(model$aic)
+  models[[name]] <- model
+}
+
+
+aic_values <- sapply(models, AIC)
+
+# Create a data frame
+df_aic <- data.frame(Model_Name = names, AIC = aic_values)
+df_aic
+# Assuming df is your DataFrame
+df_sorted <- df_aic[order(df_aic$AIC, decreasing=TRUE), ]
+df_sorted
+
+# compute nested models
+names_ord <- df_sorted$Model_Name
+names_nested <- c()
+nest_models <- list()
+
+for (name in names_ord) {
+  print(paste('name:', name))
+  names_nested <- c(names_nested, name)
+  print(names_nested)
+  formula_str <- paste("Response", "~", paste(names_nested, collapse = " + "))
+  formula <- as.formula(formula_str)
+  model <- glm(formula, data = df, family = binomial)
+  nest_models[[name]] <- model
+}
+aic_values2 <- sapply(nest_models, AIC)
+df_aic2 <- data.frame(Model_Name = names_ord, AIC = aic_values2)
+df_aic2
+# Assuming df is your DataFrame
+df_sorted2 <- df_aic2[order(df_aic2$AIC, decreasing=TRUE), ]
+df_sorted2
